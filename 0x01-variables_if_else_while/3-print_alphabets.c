aea3e7e3eb7bb4a3060920ac78497cd3abfed714
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-17 18:53:20 -0700

    Variables and conditions task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(vid)
+{
+	char ch = 'a';
+	char CH = 'A';
+	
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
