540b329db4f94b8a122596671d1965e972ae5f49
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-17 20:45:00 -0700

    Variables and conditions task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 0, vh;
+
+	while (ch <= 9)
+	{
+		vh = 0;
+
+		while (vh <= 9)
+		{
+			if (ch != vh && ch < vh)
+			{
+				putchar(ch + 48);
+				putchar(vh + 48);
+				if (ch + vh != 12)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			vh++;
+		}
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
