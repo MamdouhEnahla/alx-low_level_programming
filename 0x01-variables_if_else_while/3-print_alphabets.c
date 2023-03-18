45e52acaa656edb26661b5a663a7f6f2e689a200
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-17 18:59:25 -0700

    Variables and conditions task 3 betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,23 +6,23 @@
  *
  */
 
-int main(vid)
+int main(void)
 {
 	char ch = 'a';
 	char CH = 'A';
-	
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	
+
 	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
