a06a348e648860781dd310c551040061b9ffb721
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-21 19:32:00 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    char str[8] = "_putchar";
-    int i;
-    
-    for (i = 0; i < 8; i++)
-        _putchar(str[i]);
-    putchar('\n');
-    
-    return (0);
+	char str[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+	return (0);
 }
