bf16eaade0d5a4505ae96086d12dae3c26189cba
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-22 13:22:19 -0700

    Task betty doc

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,11 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n == 98)
-	{
-		_putchar(n);
-	}
-	else if (n < 98)
-	{
-		for (i = 0; i <= 98; i++)
-		{
-			_putchar(i);
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
+	if (n < 98)
+		for (i = n; i < 98; i++)
+			printf("%i, ", i);
 	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			_putchar(i);
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
+		for (i = n; i > 98; i--)
+			printf("%i, ", i);
+	printf("98\n");
 }
