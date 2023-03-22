81a3ace75039c12c84f1b4d3e12f6a0e968ed0d7
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-22 10:29:30 -0700

    Task betty doc

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * isalpha - check code
+ * _isalpha - check code
  *
  * @c: function argument
  *
  * Return: 1 for lower 0 otherwise.
  *
  */
-int isalpha(int c)
+int _isalpha(int c)
 {
 	if (c >= 65 && c <= 122)
 		return (1);
