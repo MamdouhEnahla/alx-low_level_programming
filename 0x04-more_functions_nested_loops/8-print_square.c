e93407d544db0a4aa4f36c13b71be32a9a60ba27
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-24 00:26:20 -0700

    Task8 logic commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - Check code
+ * @size: function argument
+ */
+void print_square(int size)
+{
+	int i = 0, j;
+	
+	while (i < size)
+	{
+		if (size <= 1)
+			break;
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
