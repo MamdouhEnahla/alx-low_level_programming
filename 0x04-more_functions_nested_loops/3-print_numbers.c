d05ee57d84736949b558be800add468046b23288
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-23 18:29:00 -0700

    Task3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Check code
+ *
+ * Description: print from 0 to 9.
+ */
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+}
