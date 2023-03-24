40db2df412bc189631203a8a98dd8b8d59b8c0c9
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-23 19:30:49 -0700

    Task4 commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Check code
+ *
+ * Description: print 0 - 9 except 2, 4.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 2 || i == 4)
+			continue
+		_putchar(i);
+	}
+	_putchar("\n");
+}
