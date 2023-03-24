132d4e091826fe86f0ddc42468c64aac03abdfbb
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-24 00:58:48 -0700

    Task9 commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * fizz_buss - Check code
- * */
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+		n++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
