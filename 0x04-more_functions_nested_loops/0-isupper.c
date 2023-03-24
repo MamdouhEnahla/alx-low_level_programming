d178e403a7f938970ff7188d3d3620312fbc4a81
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-23 17:15:44 -0700

    Task0 commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Check code
+ * @c: function argument
+ *
+ * Return:1 if upper 0 otherwise.
+ */
+int _isupper(char c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
