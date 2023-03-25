d00c5e100f5a9344f3f633945f480a28f6fb0170
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-25 02:06:02 -0700

    Task12 syntax errors

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,14 @@
  */
 void print_number(int n)
 {
-	unsigned int n = n;
+	unsigned int un = n;
 
-	if (n < 0)
+	if (un < 0)
 	{
 		_putchar('-');
-		n = -n;
+		un = -un;
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + 48);
+	if ((un / 10) > 0)
+		print_number(un / 10);
+	_putchar((un % 10) + 48);
 }
