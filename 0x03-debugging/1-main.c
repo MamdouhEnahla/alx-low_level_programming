710ce144f6830c45ca4e77cde6bde32dd8309893
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-25 00:35:24 -0700

    Task1 betty

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Check code
- *
- * Return: always 0.
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
-	int i = 0;
+	int i;
 
 	printf("Infinite loop incoming :(\n");
 
+	i = 0;
+
 	/*while (i < 10)*/
+	/*{*/
 	/*	putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
