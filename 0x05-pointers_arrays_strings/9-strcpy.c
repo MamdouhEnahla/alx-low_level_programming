05d999bdb7850ecde5c096c5ad33f45f9faa6071
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-27 20:53:52 -0700

    Betty typo fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,7 +5,7 @@
  * @dest: first arg
  * @src: second arg
  *
- * Retrun: a pointer that refers to the given string.
+ * Return: a pointer that refers to the given string.
  */
 char *_strcpy(char *dest, char *src)
 {
