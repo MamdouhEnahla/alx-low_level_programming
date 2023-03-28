973b3ed600644df4f64840ba530efcd983d3754e
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-27 19:31:27 -0700

    task7 commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - Check code
+ * @str: function argument
+ */
+void puts_half(char *str)
+{
+	int indx = 0;
+
+	while (str[indx])
+		indx++;
+
+	indx++;
+	
+	for (indx / 2; str[indx] != '\0'; indx++)
+		_putchar(str[indx]);
+	_putchar('\n');
+}
