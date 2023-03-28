87e0a6a9e639efa5797f253354822ad91200295f
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-27 18:58:28 -0700

    task6 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Check code
+ * @str: function argument
+ */
+void puts2(char *str)
+{
+	int indx = 0;
+
+	while (str[indx] != '\0')
+	{
+		if (indx % 2 == 0)
+			_putchar(str[indx]);
+	}
+	_putchar('\n');
+}
