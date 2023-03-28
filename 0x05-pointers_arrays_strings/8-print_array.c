a3dafbc763c86aaec96ea113c56a38297ba4d150
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-27 19:57:37 -0700

    task9 commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,13 +5,13 @@
  * @a: first argument
  * @n: second argument
  */
-void print_array(int *a, int *n)
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == n - 1)
+		if (i == (n - 1))
 			printf("%d", a[i]);
 		else
 			printf("%d ", a[i]);
