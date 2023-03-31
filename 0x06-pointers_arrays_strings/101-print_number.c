eb4a463152fe36e20ea5b6778eced3b34a63525b
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-30 17:20:06 -0700

    Task9 syntax commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,17 @@
  */
 void print_number(int n)
 {
-	unsigned int s;
-
-	s = n;
+	unsigned int s = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		s = -n;
 	}
+
 	if (s / 10 != 0)
+	{	
 		print_number(s / 10);
+	}
 	_putchar((s % 10) + '0');
 }
