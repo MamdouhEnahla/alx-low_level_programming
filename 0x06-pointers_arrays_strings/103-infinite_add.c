e4eec201c3fe2e37ead90c514de3d734a0d04229
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-03-30 18:28:05 -0700

    Task11 syntax commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,34 +1,36 @@
 #include "main.h"
 
 /**
- * infinite_add - Chheck code
- * @n1: first arg
- * @n2: second arg
- * @r: third arg
- * @size_r: last arg
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
  *
- * Return: char value
- */
+*/
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, l = 0, d = 0, k, f, s;
+	/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
 	while (n1[i] != '\0')
 		i++;
 	while (n2[j] != '\0')
 		j++;
 	if (i > j)
-	       l = i;
+		l = i;
 	else
 		l = j;
 	if (l + 1 > size_r)
 		return (0);
 	r[l] = '\0';
-	for (k = l - 1; k >= 0; k--)
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
 		i--;
 		j--;
-		if (i >= 0) 
+		if (i >= 0)
 			f = n1[i] - '0';
 		else
 			f = 0;
@@ -37,7 +39,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			s = 0;
 		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) % 10 + '\0';
+		d = (f + s + d) / 10;
 	}
 	if (d == 1)
 	{
