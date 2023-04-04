98a52b60fe8d8a769d52e4cc2bcda948267cd532
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-04-03 20:17:07 -0700

    Task6 commit.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Check code.
+ * @a: function arg.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
