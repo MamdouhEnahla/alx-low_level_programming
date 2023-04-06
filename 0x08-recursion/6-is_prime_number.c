34d7d39b6b6f8f0328a4b16aaff1676be4900f37
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-04-05 19:13:44 -0700

    Task6 commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check code
+ * @n: function arg
+ *
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - check code
+ * @n: first arg
+ * @p: second arg
+ *
+ * Return: integer
+ */
+int check_prime(int n, int p)
+{
+	if (p >= n && n > 1)
+		return (1);
+	else if (n % p == 0 || n <= 1)
+		return (0);
+	return (check_prime(n, p + 1));
+}
