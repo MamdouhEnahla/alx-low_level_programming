8c694cd3c769ce7f51b4d38224ad68395bd2f6d0
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-04-05 19:47:30 -0700

    Task6 typo commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - check code
- * @n: function arg
- *
- * Return: 0 or 1
- */
-int is_prime_number(int n)
-{
-	return (check_prime(n, 2));
-}
-
-/**
  * check_prime - check code
  * @n: first arg
  * @p: second arg
@@ -20,9 +9,19 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int p)
 {
-	if (p >= n && n > 1)
-		return (1);
-	else if (n % p == 0 || n <= 1)
-		return (0);
-	return (check_prime(n, p + 1));
+        if (p >= n && n > 1)
+                return (1);
+        else if (n % p == 0 || n <= 1)
+                return (0);
+        return (check_prime(n, p + 1));
+}
+/**
+ * is_prime_number - check code
+ * @n: function arg
+ *
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
 }
