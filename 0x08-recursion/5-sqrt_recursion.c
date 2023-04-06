1c7d911a7c0ee86c3d24147d8b45599b49de2a90
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-04-05 19:02:57 -0700

    Task5 commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt - Check code.
+ * @n: first arg
+ * @val: second arg
+ *
+ * Return: integer value.
+ */
+int _sqrt(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	return (-1);
+}
+/**
+ * _sqrt_recursion - Check code
+ * @n: function arg
+ *
+ * Return: integer value.
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
