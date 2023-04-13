fd529e598ac38a8101e89c79dd960ac4e6378f70
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-04-12 19:48:02 -0700

    Memory task4

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * **alloc_grid - Check code.
+ * @width: first arg
+ * @height: second arg
+ *
+ * Return: a pointer to a 2d array of integers.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	arr = malloc(sizeof(*arr) * height);
+	if (width <= 0 || height <= 0 || arr == 0)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(**arr) * width);
+		if (ar[i] == 0)
+		{
+			while (i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+
+	return (arr);
+}
