dcf8c7c440cc4f3f2fc5c9d538d85f8bc84c77f1
mamdouhelnahla75 <mamdouh57118@gmail.com>
2023-04-28 03:42:28 +0200

    using headers

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - Check code
- * @s: function arg
- *
- * Return: number of bytes in the string pointed  to  by s.
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	if (*s)
-		len += _strlen_(s + 1) + 1;
-
-	return (len);
-}
-
-/**
  * print_list - Prints all the elements of a list_t list
  * @h: a pointer to the head
  *
@@ -29,9 +13,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%d] %s\n", _strlen(h->str), h->str);
+			printf("[%d] %s\n", strlen(h->str), h->str);
 		else
-			printf("[%d] %s\n", _strlen(h->str), "(nil)");
+			printf("[%d] %s\n", strlen(h->str), "(nil)");
 		sz++;
 		h = h->next;
 	}
