9e8c79c1d9994b2b00c6e38b5afbd68bac4bd55e
MamdouhEnahla <mamdouh57118@gmail.com>
2023-05-09 04:52:27 +0200

    File operations

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * create_file - check code
+ * @filename: first arg
+ * @text_content: second arg
+ *
+ * Return: 1 OR -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t n_bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+
+	if (len)
+		n_bytes = write(fd, text_content, len);
+
+	close(fd);
+
+	return (n_bytes == len ? 1 : -1);
+}
+
+/**
+ * _strlen - Check code
+ * @s: function arg
+ *
+ * Return: integer value.
+ */
+int _strlen(char *s)
+{
+	int ptr = 0;
+
+	if (*s)
+		ptr += _strlen(s + 1) + 1;
+
+	return (ptr);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * append_text_to_file - appends a text to a file
+ * @filename: first arg
+ * @text_content: second arg
+ *
+ * Return: 1 OR -1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t n_bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+	if (len)
+		n_bytes = write(fd, text_content, len);
+
+	close(fd);
+
+	return (n_bytes == len ? 1 : -1);
+}
+
+/**
+ * _strlen - Check code
+ * @s: function arg
+ *
+ * Return: integer value.
+ */
+int _strlen(char *s)
+{
+	int ptr = 0;
+
+	if (*s)
+		ptr += _strlen(s + 1) + 1;
+
+	return (ptr);
+}
