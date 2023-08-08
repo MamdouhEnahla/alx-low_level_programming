91fdca83ea00a1c59f1b8e3ebf0a0f50f3f87c12
MamdouhEnahla <mamdouh57118@gmail.com>
2023-08-08 04:03:04 +0200

    task again

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,11 +14,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 
 	buf = malloc(letters);
-	if (!buf || !filename || !letters)
+	if (!buf || !filename || letters == 0)
 		return (0);
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	br = read(fd, buf, letters);
 	bw = write(STDOUT_FILENO, buf, br);
 	free(buf);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,26 +3,10 @@
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#define READ_BUF_SIZE 1024
-
-/**
- * _strlen - Check code
- * @s: function arg
- *
- * Return: integer value
- */
-int _strlen(char *s)
-{
-	int ptr = 0;
-
-	if (*s)
-		ptr += _strlen(s + 1) + 1;
-
-	return (ptr);
-}
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
