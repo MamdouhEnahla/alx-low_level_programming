55d69f9e4fcc13807f772993e3aed2798dcfd235
MamdouhEnahla <mamdouh57118@gmail.com>
2023-08-08 04:48:30 +0200

    task1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,17 +9,16 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
-	ssize_t b = 0;
+	int fd, len = 0, b = 0;
 
-	len = strlen(text_content);
 	if (!filename)
-		return (-1);
+                return (-1);
+	len = strlen(text_content);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
 	if (len)
 		b = write(fd, text_content, len);
+	if (fd == -1 || b == -1)
+		return (-1);
 	close(fd);
-	return (b == len ? 1 : -1);
+	return (1);
 }
