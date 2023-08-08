2aef22b57b39dac485579479adb90e3ce1497eef
MamdouhEnahla <mamdouh57118@gmail.com>
2023-08-08 04:50:15 +0200

    task1 betty fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, len = 0, b = 0;
 
 	if (!filename)
-                return (-1);
+		return (-1);
 	len = strlen(text_content);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (len)
