0bb426a05c4211fc3ad8e8466678a8591758c259
MamdouhEnahla <mamdouh57118@gmail.com>
2023-08-08 02:26:19 +0200

    fix try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int _strlen(char *s);
 /**
  * create_file - Check code
  * @filename: file name
@@ -9,19 +10,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes = 0, len = _strlen(text_content);
+	int fd, bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	if (fd == -1)
-		return (-1);
-	if (len)
-		bytes = write(fd, text_content, len);
-
-	close(fd);
-	return (bytes == len ? 1 : -1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+        if (len)
+                bytes = write(fd, text_content, len);
+        if (fd == -1 || bytes == -1)
+                return (-1);
+        close(fd);
+        return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int _strlen(char *s);
 /**
  * append_text_to_file - Check code
  * @filename: file name
@@ -9,18 +10,15 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes = 0, len = _strlen(text_content);
+	int fd, bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
-
+	if (fd == -1 || bytes == -1)
+                return (-1);
 	close(fd);
-	return (bytes == len ? 1 : -1);
+	return (1);
 }
